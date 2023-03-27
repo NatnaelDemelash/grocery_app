4a51ce63190db2491e5ac010e77db1e7822da949
Natnael Demelash <natidemelash074@gmail.com>
2023-03-27 16:10:51 +0300

    clear item and filter option added

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const itemForm = document.getElementById("item-form"); //form
 const itemInput = document.getElementById("item-input"); // user inpur
 const itemList = document.getElementById("item-list"); // ul
+const clearBtn = document.getElementById("clear"); // clear button
+const itemFilter = document.getElementById("filter");
 
 // Add grocery items to the list
 function addItem(e) {
@@ -20,7 +22,10 @@ function addItem(e) {
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
 
+  // Add li to the DOM list
   itemList.appendChild(li);
+
+  checkUI();
 
   itemInput.value = "";
 }
@@ -41,4 +46,53 @@ const createIcon = (classes) => {
   return icon;
 };
 
+// Remove Items from the list
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+// Clear All Items
+function clearItems() {
+  itemList.innerHTML = "";
+  checkUI();
+}
+
+// Filter Item
+function filterItem(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLocaleLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+const checkUI = () => {
+  const items = itemList.querySelectorAll("li");
+  if (items.length == 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+};
+
+// Event Listners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItem);
+
+checkUI();
